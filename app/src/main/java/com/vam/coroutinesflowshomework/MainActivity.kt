4ef0a894b82e0932be4384357cc0d7e78acb58a2
Vîlcu Ana-Maria <anna.ghizdavat@gmail.com>
2026-02-03 14:02:41 +0100

    Base android project added

---
+++ app/src/main/java/com/vam/coroutinesflowshomework/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.vam.coroutinesflowshomework
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.vam.coroutinesflowshomework.ui.theme.CoroutinesFlowsHomeworkTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CoroutinesFlowsHomeworkTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
