d90f0636eb806a3765cafc3db1cfe2a9a6481649
Vîlcu Ana-Maria <anna.ghizdavat@gmail.com>
2026-02-09 16:25:12 +0100

    Coroutine Error Handling [Medium] Homework done

--- app/src/main/java/com/vam/coroutinesflowshomework/MainActivity.kt
+++ app/src/main/java/com/vam/coroutinesflowshomework/MainActivity.kt
@@ -4,21 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
+import com.vam.coroutinesflowshomework.homework.EmailService
 import com.vam.coroutinesflowshomework.ui.theme.CoroutinesFlowsHomeworkTheme
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            CoroutinesFlowsHomeworkTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-                }
-            }
+        val handler = CoroutineExceptionHandler { coroutineContext, throwable ->
+            throwable.printStackTrace()
+        }
+
+        lifecycleScope.launch(handler) {
+            EmailService.addToMailingList(
+                listOf(
+                    "dancing.dave@email.com",
+                    "caffeinated.coder@email.com",
+                    "bookworm.betty@email.com",
+                    "gardening.guru@email.com",
+                    "sleepy.slothemail.com",
+                    "hungry.hippo@email.com",
+                    "clueless.cathy@email.com",
+                    "techy.tom@email.com",
+                    "musical.maryemail.com",
+                    "adventurous.alice@email.com"
+                )
+            )
+            EmailService.sendNewsletter()
+            println("Done sending emails")
+        }
+
+        setContent {
+            CoroutinesFlowsHomeworkTheme {}
         }
     }
 }
