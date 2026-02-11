735f8d20b523c953f67f9fdfee305bda3a0239a2
Vîlcu Ana-Maria <anna.ghizdavat@gmail.com>
2026-02-11 15:05:23 +0100

    Combining What You’ve Learnt So Far  [Hard] Homework done

--- app/src/main/java/com/vam/coroutinesflowshomework/MainActivity.kt
+++ app/src/main/java/com/vam/coroutinesflowshomework/MainActivity.kt
@@ -1,25 +1,98 @@
 package com.vam.coroutinesflowshomework
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import com.vam.coroutinesflowshomework.ui.theme.CoroutinesFlowsHomeworkTheme
 
-class MainActivity : ComponentActivity() {
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.lifecycle.lifecycleScope
+import com.vam.coroutinesflowshomework.homework.BiometricPromptManager
+import com.vam.coroutinesflowshomework.homework.BiometricResult
+import com.vam.coroutinesflowshomework.ui.theme.CoroutinesFlowsHomeworkTheme
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeoutOrNull
+import kotlin.coroutines.cancellation.CancellationException
+
+private const val AUTHENTICATION_TIMEOUT_MILLIS = 3000L
+
+@RequiresApi(Build.VERSION_CODES.P)
+class MainActivity : AppCompatActivity() {
+
+    private val promptManager by lazy {
+        BiometricPromptManager(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        BiometricPromptManager(this)
         setContent {
             CoroutinesFlowsHomeworkTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                val context = LocalContext.current
+                var biometricsResult by remember {
+                    mutableStateOf<BiometricResult?>(null)
+                }
 
+                LaunchedEffect(biometricsResult) {
+                    if (biometricsResult != null) {
+                        Toast.makeText(context, biometricsResult.toString(), Toast.LENGTH_LONG)
+                            .show()
+                        biometricsResult = null
+                    }
+                }
+
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Button(onClick = {
+                        lifecycleScope.launch {
+                            launch {
+                                val result = withTimeoutOrNull(AUTHENTICATION_TIMEOUT_MILLIS) {
+                                    try {
+                                        biometricsResult = promptManager.showBiometricPrompt(
+                                            title = "Authentication",
+                                            description = "Please authenticate to proceed"
+                                        )
+                                    } catch (e: Exception) {
+                                        if (e is CancellationException) {
+                                            Toast.makeText(context, "Cancelled", Toast.LENGTH_LONG)
+                                                .show()
+                                            throw e
+                                        }
+                                        e.printStackTrace()
+                                    }
+                                }
+                                if (result == null) {
+                                    println("Timeout reached, cancelling...")
+                                    lifecycleScope.cancelChildren()
+                                }
+                            }
+                        }
+                    }) {
+                        Text(text = "Authenticate")
+                    }
                 }
             }
         }
     }
 }
 
+fun LifecycleCoroutineScope.cancelChildren() {
+    this.coroutineContext.cancelChildren()
+}
---
+++ app/src/main/java/com/vam/coroutinesflowshomework/homework/BiometricPromptManager.kt
@@ -0,0 +1,96 @@
+package com.vam.coroutinesflowshomework.homework
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.biometric.BiometricPrompt
+import androidx.biometric.BiometricPrompt.PromptInfo
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+
+
+@RequiresApi(Build.VERSION_CODES.P)
+class BiometricPromptManager(
+    private val activity: AppCompatActivity
+) {
+    suspend fun showBiometricPrompt(
+        title: String,
+        description: String
+    ): BiometricResult {
+        return suspendCancellableCoroutine { continuation ->
+            val manager = BiometricManager.from(activity)
+            val authenticators = if (Build.VERSION.SDK_INT >= 30) {
+                BIOMETRIC_STRONG or DEVICE_CREDENTIAL
+            } else BIOMETRIC_STRONG
+
+            val promptInfo = PromptInfo.Builder()
+                .setTitle(title)
+                .setDescription(description)
+                .setAllowedAuthenticators(authenticators)
+
+            if (Build.VERSION.SDK_INT < 30) {
+                promptInfo.setNegativeButtonText("Cancel")
+            }
+
+            when (manager.canAuthenticate(authenticators)) {
+                BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                    if (continuation.isActive) {
+                        continuation.resume(BiometricResult.HardwareUnavailable)
+                        return@suspendCancellableCoroutine
+                    }
+                }
+
+                BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                    if (continuation.isActive) {
+                        continuation.resume(BiometricResult.FeatureUnavailable)
+                        return@suspendCancellableCoroutine
+                    }
+                }
+
+                BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                    if (continuation.isActive) {
+                        continuation.resume(BiometricResult.AuthenticationNotSet)
+                        return@suspendCancellableCoroutine
+                    }
+                }
+
+                else -> Unit
+            }
+
+            val prompt = BiometricPrompt(
+                activity,
+
+                object : BiometricPrompt.AuthenticationCallback() {
+                    override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                        super.onAuthenticationError(errorCode, errString)
+                        if (continuation.isActive) {
+                            continuation.resume(BiometricResult.AuthenticationError(errString.toString()))
+                        }
+                    }
+
+                    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                        super.onAuthenticationSucceeded(result)
+                        if (continuation.isActive) {
+                            continuation.resume(BiometricResult.AuthenticationSuccess)
+                        }
+                    }
+
+                    override fun onAuthenticationFailed() {
+                        super.onAuthenticationFailed()
+                        if (continuation.isActive) {
+                            continuation.resume(BiometricResult.AuthenticationFailed)
+                        }
+                    }
+                }
+            )
+            prompt.authenticate(promptInfo.build())
+
+            continuation.invokeOnCancellation {
+                prompt.cancelAuthentication()
+            }
+        }
+    }
+}
